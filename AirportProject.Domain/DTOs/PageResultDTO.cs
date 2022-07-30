fe7d2db767675ec81b729d9a047963cbf65f8e2b
kievzenit <contact@kievzenit.tk>
2022-07-30 19:02:54 +0300

    PageResultDTO added

---
+++ AirportProject.Domain/DTOs/PageResultDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AirportProject.Domain.DTOs
+{
+    public class PageResultDTO<TItems>
+        where TItems : DTO
+    {
+        public ICollection<TItems> Items { get; private set; }
+        public int TotalCount { get => this.Items.Count; }
+
+        public PageResultDTO(ICollection<TItems> items)
+        {
+            this.Items = items;
+        }
+    }
+}
