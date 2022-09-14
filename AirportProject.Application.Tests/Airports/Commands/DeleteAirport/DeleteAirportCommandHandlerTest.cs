6188cd121892bf3ed8e4f79e53c0d8cbbbb283b8
kievzenit <contact@kievzenit.tk>
2022-09-14 14:56:49 +0300

    DeleteAirportCommand handler and validator tests

---
+++ AirportProject.Application.Tests/Airports/Commands/DeleteAirport/DeleteAirportCommandHandlerTest.cs
@@ -0,0 +1,74 @@
+﻿using AirportProject.Application.Airports.Commands.DeleteAirport;
+using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Airports.Commands.DeleteAirport
+{
+    [TestClass]
+    public class DeleteAirportCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_HandleMethod_When_DataIsValid_Then_ShouldFinishedWithoutExceptions()
+        {
+            // arrange
+            var mockRepository = new Mock<IAirportRepository>(MockBehavior.Strict);
+
+            var command = new DeleteAirportCommand(5);
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            mockRepository.Setup(f => f.Delete(command.Id, cancellationToken)).ReturnsAsync(true);
+
+            var handler = new DeleteAirportCommandHandler(mockRepository.Object);
+
+            // act
+            var result = handler.Handle(command, cancellationToken).Result;
+
+            // assert
+            Assert.IsNotNull(result);
+            mockRepository.Verify(f => f.Delete(command.Id, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_HandleMethod_When_DataIsInValid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IAirportRepository>(MockBehavior.Strict);
+
+            var command = new DeleteAirportCommand(-5);
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            var handler = new DeleteAirportCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(command, cancellationToken));
+        }
+
+        [TestMethod]
+        public void Test_HandleMethod_When_DataIsValidAndAirportDoesNotExist_Then_ShouldThrowNotFoundException()
+        {
+            // arrange
+            var mockRepository = new Mock<IAirportRepository>(MockBehavior.Strict);
+
+            var command = new DeleteAirportCommand(5);
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            mockRepository.Setup(f => f.Delete(command.Id, cancellationToken)).ReturnsAsync(false);
+
+            var handler = new DeleteAirportCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<NotFoundException>(() => handler.Handle(command, cancellationToken));
+            mockRepository.Verify(f => f.Delete(command.Id, cancellationToken), Times.Once);
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Airports/Commands/DeleteAirport/DeleteAirportCommandValidatorTest.cs
@@ -0,0 +1,40 @@
+﻿using AirportProject.Application.Airports.Commands.DeleteAirport;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportProject.Application.Tests.Airports.Commands.DeleteAirport
+{
+    [TestClass]
+    public class DeleteAirportCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_DataIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var command = new DeleteAirportCommand(5);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_DataIsInValid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new DeleteAirportCommand(-5);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
