51a449dc8ad7ee040bf12469e862290e1a76a99c
kievzenit <contact@kievzenit.tk>
2022-09-14 16:57:23 +0300

    GetAirportsWithPaginationQuery handler and validator tests

---
+++ AirportProject.Application.Tests/Airports/Queries/GetAirportsWithPagination/GetAirportWithPaginationQueryHandlerTest.cs
@@ -0,0 +1,77 @@
+﻿using AirportProject.Application.Airports.Queries.GetAirportsWithPagination;
+using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.DTOs;
+using AirportProject.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AirportProject.Application.Tests.Airports.Queries.GetAirportsWithPagination
+{
+    [TestClass]
+    public class GetAirportWithPaginationQueryHandlerTest
+    {
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryIsValid_Then_ShouldReturnValidPageResultDTO()
+        {
+            // arrange
+            var mockRepository = new Mock<IAirportRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Airport, AirportDTO>>(MockBehavior.Strict);
+            
+            var query = new GetAirportsWithPaginationQuery(5);
+            var airports = (ICollection<Airport>)new List<Airport>();
+            var airportsDTO = (ICollection<AirportDTO>)new List<AirportDTO>();
+
+            const int totalAirportsCount = 50;
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            mockRepository.Setup(f => f.GetRange(query.PageNumber, query.PageSize, cancellationToken))
+                .ReturnsAsync(airports);
+            mockRepository.Setup(f => f.GetTotalCount(cancellationToken)).ReturnsAsync(totalAirportsCount);
+            mockCaster.Setup(f => f.Cast(airports, cancellationToken)).ReturnsAsync(airportsDTO);
+
+            var handler = new GetAirportsWithPaginationQueryHandler(mockRepository.Object, mockCaster.Object);
+
+            var expectedItems = airportsDTO;
+            var expectedCount = totalAirportsCount;
+
+            // act
+            var result = handler.Handle(query, cancellationToken).Result;
+
+            // assert
+            Assert.AreEqual(expectedItems, result.Items);
+            Assert.AreEqual(expectedCount, result.TotalCount);
+
+            mockRepository.Verify(f => 
+                f.GetRange(query.PageNumber, query.PageSize, cancellationToken), 
+                Times.Once);
+            mockRepository.Verify(f => f.GetTotalCount(cancellationToken), Times.Once);
+            mockCaster.Verify(f => f.Cast(airports, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryIsInvalid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IAirportRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Airport, AirportDTO>>(MockBehavior.Strict);
+
+            var query = new GetAirportsWithPaginationQuery(-5);
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            var handler = new GetAirportsWithPaginationQueryHandler(mockRepository.Object, mockCaster.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(query, cancellationToken));
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Airports/Queries/GetAirportsWithPagination/GetAirportsWithPaginationQueryValidatorTest.cs
@@ -0,0 +1,35 @@
+﻿using AirportProject.Application.Airports.Queries.GetAirportsWithPagination;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Airports.Queries.GetAirportsWithPagination
+{
+    [TestClass]
+    public class GetAirportsWithPaginationQueryValidatorTest
+    {
+        [TestMethod]
+        public void Test_QueryValidator_When_InputDataIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var query = new GetAirportsWithPaginationQuery(5);
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryValidator_When_InputDataIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var query = new GetAirportsWithPaginationQuery(-5);
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
