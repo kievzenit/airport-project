324037b99febbc50962e5f3f243b9a3c297b2ec2
kievzenit <contact@kievzenit.tk>
2022-09-14 15:31:38 +0300

    UpdateAirportCommand handler and validator tests

---
+++ AirportProject.Application.Tests/Airports/Commands/UpdateAirport/UpdateAirportCommandHandlerTest.cs
@@ -0,0 +1,92 @@
+﻿using AirportProject.Application.Airports.Commands.UpdateAirport;
+using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Airports.Commands.UpdateAirport
+{
+    [TestClass]
+    public class UpdateAirportCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_CommandHandler_When_InputDataIsValidAndAirportExists_Then_ShouldFinishedWithoutExceptions()
+        {
+            // arrange
+            var mockRepository = new Mock<IAirportRepository>(MockBehavior.Strict);
+
+            var command = new UpdateAirportCommand
+            {
+                Id = 24,
+                Name = "Kiev",
+                City = "Borispol",
+                Country = "Ukraine"
+            };
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            mockRepository.Setup(f => f.Update(command, cancellationToken)).ReturnsAsync(true);
+
+            var handler = new UpdateAirportCommandHandler(mockRepository.Object);
+
+            // act
+            var result = handler.Handle(command, cancellationToken).Result;
+
+            // assert
+            Assert.IsNotNull(result);
+            mockRepository.Verify(f => f.Update(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_InputDataIsInValid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IAirportRepository>(MockBehavior.Strict);
+
+            var command = new UpdateAirportCommand
+            {
+                Id = -24,
+                Name = new string('a', 51),
+                City = "Borispol",
+                Country = "Ukraine"
+            };
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            var handler = new UpdateAirportCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(command, cancellationToken));
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_InputDataIsValidAndAirportDoesNotExist_Then_ShouldThrowNotFoundException()
+        {
+            // arrange
+            var mockRepository = new Mock<IAirportRepository>(MockBehavior.Strict);
+
+            var command = new UpdateAirportCommand
+            {
+                Id = 24,
+                Name = "Kiev",
+                City = "Borispol",
+                Country = "Ukraine"
+            };
+
+            var cancellationSource = new CancellationTokenSource();
+            var cancellationToken = cancellationSource.Token;
+
+            mockRepository.Setup(f => f.Update(command, cancellationToken)).ReturnsAsync(false);
+
+            var handler = new UpdateAirportCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<NotFoundException>(() => handler.Handle(command, cancellationToken));
+            mockRepository.Verify(f => f.Update(command, cancellationToken), Times.Once);
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Airports/Commands/UpdateAirport/UpdateAirportCommandValidatorTest.cs
@@ -0,0 +1,104 @@
+﻿using AirportProject.Application.Airports.Commands.UpdateAirport;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Airports.Commands.UpdateAirport
+{
+    [TestClass]
+    public class UpdateAirportCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_DataIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var command = new UpdateAirportCommand
+            {
+                Id = 89,
+                Name = "Kiev",
+                City = "Borispol",
+                Country = "Ukraine"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_AirportIdIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdateAirportCommand
+            {
+                Id = -89,
+                Name = "Kiev",
+                City = "Borispol",
+                Country = "Ukraine"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_AirportNameIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdateAirportCommand
+            {
+                Id = 89,
+                Name = new string('a', 51),
+                City = "Borispol",
+                Country = "Ukraine"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_AirportCityIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdateAirportCommand
+            {
+                Id = 89,
+                Name = "Kiev",
+                City = new string('a', 51),
+                Country = "Ukraine"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_AirportCountryIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdateAirportCommand
+            {
+                Id = 89,
+                Name = "Kiev",
+                City = "Borispol",
+                Country = new string('a', 51)
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
