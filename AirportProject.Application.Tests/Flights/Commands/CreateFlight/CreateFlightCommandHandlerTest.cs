847088d91efc4f4bba445d95520ea616cb295e17
kievzenit <contact@kievzenit.tk>
2022-09-15 18:14:11 +0300

    CreateFlightCommand handler and validator tests

---
+++ AirportProject.Application.Tests/Flights/Commands/CreateFlight/CreateFlightCommandHandlerTest.cs
@@ -0,0 +1,118 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.DTOs;
+using AirportProject.Application.Flights.Commands.CreateFlight;
+using AirportProject.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Flights.Commands.CreateFlight
+{
+    [TestClass]
+    public class CreateFlightCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_CommandHandler_When_InputDataIsValid_Then_ShouldReturnFlightDTO()
+        {
+            // arrange
+            var mockRepository = new Mock<IFlightRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Flight, FlightDTO>>(MockBehavior.Strict);
+
+            var kievAirport = new Airport
+            {
+                Id = 8,
+                Name = "Kiev",
+                City = "Borispol",
+                Country = "Ukraine"
+            };
+            var berlinAirport = new Airport
+            {
+                Id = 19,
+                Name = "Berlin",
+                City = "Berlin",
+                Country = "Germany"
+            };
+
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = kievAirport.Name,
+                DepartureAirportName = berlinAirport.Name,
+                Terminal = 'G',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("2/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            var flight = new Flight
+            {
+                Id = 289,
+                ArrivalAirport = kievAirport,
+                DepartureAirport = berlinAirport,
+                Terminal = command.Terminal,
+                ArrivalTime = command.ArrivalTime,
+                DepartureTime = command.DepartureTime,
+                Status = command.Status
+            };
+
+            var flightDTO = new FlightDTO
+            {
+                Id = flight.Id,
+                ArrivalAirportName = flight.ArrivalAirport.Name,
+                DepartureAirportName = flight.DepartureAirport.Name,
+                Terminal = flight.Terminal,
+                ArrivalTime = flight.ArrivalTime,
+                DepartureTime = flight.DepartureTime,
+                Status = flight.Status,
+                EconomyPrice = command.EconomyPrice,
+                BusinessPrice = command.BusinessPrice
+            };
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.Create(command, cancellationToken)).ReturnsAsync(flight);
+            mockCaster.Setup(f => f.Cast(flight, cancellationToken)).ReturnsAsync(flightDTO);
+
+            var handler = new CreateFlightCommandHandler(mockRepository.Object, mockCaster.Object);
+
+            var expectedResult = flightDTO;
+
+            // act
+            var actualResult = handler.Handle(command, cancellationToken).Result;
+
+            // assert
+            Assert.AreEqual(expectedResult, actualResult);
+            mockRepository.Verify(f => f.Create(command, cancellationToken), Times.Once);
+            mockCaster.Verify(f => f.Cast(flight, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_InputDataIsInvalid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IFlightRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Flight, FlightDTO>>(MockBehavior.Strict);
+
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = '8',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("2/09/15 09:00"),
+                Status = "notexisting",
+                EconomyPrice = 10,
+                BusinessPrice = 1200
+            };
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            var handler = new CreateFlightCommandHandler(mockRepository.Object, mockCaster.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(command, cancellationToken));
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Flights/Commands/CreateFlight/CreateFlightCommandValidatorTest.cs
@@ -0,0 +1,309 @@
+﻿using AirportProject.Application.Flights.Commands.CreateFlight;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AirportProject.Application.Tests.Flights.Commands.CreateFlight
+{
+    [TestClass]
+    public class CreateFlightCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_DataIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_ArrivalAirportNameIsTooBig_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = new string('a', 51),
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_ArrivalAirportNameIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = string.Empty,
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_DepartureAirportNameIsTooBig_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = new string('a', 51),
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_DepartureAirportNameIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = string.Empty,
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_TerminalIsNotValidCharacter_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = '6',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_ArrivalTimeIsEqualToDepartureTime_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 15:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_ArrivalTimeIsLessThanToDepartureTime_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 10:00"),
+                DepartureTime = DateTime.Parse("22/09/15 15:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_StatusIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "notexisting",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_EconomyPriceIsTooBig_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000000,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_EconomyPriceIsTooSmall_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 10,
+                BusinessPrice = 1200
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_BusinessPriceIsTooBig_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 1200000
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_BusinessPriceIsTooSmall_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new CreateFlightCommand
+            {
+                ArrivalAirportName = "Kiev",
+                DepartureAirportName = "Berlin",
+                Terminal = 'K',
+                ArrivalTime = DateTime.Parse("22/09/15 15:00"),
+                DepartureTime = DateTime.Parse("22/09/15 09:00"),
+                Status = "normal",
+                EconomyPrice = 1000,
+                BusinessPrice = 12
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
