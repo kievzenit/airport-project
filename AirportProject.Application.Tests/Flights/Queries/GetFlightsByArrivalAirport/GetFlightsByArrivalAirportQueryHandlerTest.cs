de2f30d0f602047d9fee65a96b9dbd6e9d918589
kievzenit <contact@kievzenit.tk>
2022-09-16 15:47:50 +0300

    GetFlightsByArrivalAirportQuery handler and validator tests

---
+++ AirportProject.Application.Tests/Flights/Queries/GetFlightsByArrivalAirport/GetFlightsByArrivalAirportQueryHandlerTest.cs
@@ -0,0 +1,63 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.DTOs;
+using AirportProject.Application.Flights.Queries.GetFlightsByArrivalAirport;
+using AirportProject.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Flights.Queries.GetFlightsByArrivalAirport
+{
+    [TestClass]
+    public class GetFlightsByArrivalAirportQueryHandlerTest
+    {
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryIsValid_Then_ShouldReturnICollectionOfFlightDTO()
+        {
+            // arrange
+            var mockRepository = new Mock<IFlightRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Flight, FlightDTO>>(MockBehavior.Strict);
+
+            var query = new GetFlightsByArrivalAirportQuery("Kiev");
+
+            var flights = new List<Flight>();
+            var flightDTOs = new List<FlightDTO>();
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.SearchByFlightArrivalAirport(query, cancellationToken))
+                .ReturnsAsync(flights);
+            mockCaster.Setup(f => f.Cast(flights, cancellationToken)).ReturnsAsync(flightDTOs);
+
+            var handler = new GetFLightsByArrivalAirportQueryHandler(mockRepository.Object, mockCaster.Object);
+
+            var expectedResult = flightDTOs;
+
+            // act
+            var actualResult = handler.Handle(query, cancellationToken).Result;
+
+            // assert
+            Assert.AreEqual(expectedResult, actualResult);
+            mockRepository.Verify(f => f.SearchByFlightArrivalAirport(query, cancellationToken), Times.Once);
+            mockCaster.Verify(f => f.Cast(flights, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryIsInvalid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IFlightRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Flight, FlightDTO>>(MockBehavior.Strict);
+
+            var query = new GetFlightsByArrivalAirportQuery("Kiev");
+            var handler = new GetFLightsByArrivalAirportQueryHandler(mockRepository.Object, mockCaster.Object);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(query, cancellationToken));
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Flights/Queries/GetFlightsByArrivalAirport/GetFlightsByArrivalAirportQueryValidatorTest.cs
@@ -0,0 +1,48 @@
+﻿using AirportProject.Application.Flights.Queries.GetFlightsByArrivalAirport;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Flights.Queries.GetFlightsByArrivalAirport
+{
+    [TestClass]
+    public class GetFlightsByArrivalAirportQueryValidatorTest
+    {
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryIsValid_Then_ShouldreturnTrue()
+        {
+            // arrange
+            var query = new GetFlightsByArrivalAirportQuery("Kiev");
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryAirportNameIsTooBig_Then_ShouldreturnFalse()
+        {
+            // arrange
+            var query = new GetFlightsByArrivalAirportQuery(new string('a', 51));
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryAirportNameIsEmpty_Then_ShouldreturnFalse()
+        {
+            // arrange
+            var query = new GetFlightsByArrivalAirportQuery(string.Empty);
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
