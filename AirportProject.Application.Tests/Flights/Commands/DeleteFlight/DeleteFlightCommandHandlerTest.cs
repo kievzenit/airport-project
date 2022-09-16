1eff2b34bbccd6fb83496de4ce10d0146300cf78
kievzenit <contact@kievzenit.tk>
2022-09-16 15:10:34 +0300

    DeleteFLightCommand handler and validator tests

---
+++ AirportProject.Application.Tests/Flights/Commands/DeleteFlight/DeleteFlightCommandHandlerTest.cs
@@ -0,0 +1,71 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.Exceptions;
+using AirportProject.Application.Flights.Commands.DeleteFlight;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Flights.Commands.DeleteFlight
+{
+    [TestClass]
+    public class DeleteFlightCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_CommandHandler_When_InputDataIsValidAndFlightExists_Then_ShouldFinishedWithoutExceptions()
+        {
+            // arrange
+            var mockRepository = new Mock<IFlightRepository>(MockBehavior.Strict);
+
+            var command = new DeleteFlightCommand(9);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.Delete(command, cancellationToken)).ReturnsAsync(true);
+
+            var handler = new DeleteFlightCommandHandler(mockRepository.Object);
+
+            // act
+            var result = handler.Handle(command, cancellationToken).Result;
+
+            // assert
+            Assert.IsNotNull(result);
+            mockRepository.Verify(f => f.Delete(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_InputDataIsValidAndDoesNotFlightExist_Then_ShouldThrowNotFoundException()
+        {
+            // arrange
+            var mockRepository = new Mock<IFlightRepository>(MockBehavior.Strict);
+
+            var command = new DeleteFlightCommand(9);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.Delete(command, cancellationToken)).ReturnsAsync(false);
+
+            var handler = new DeleteFlightCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<NotFoundException>(() => handler.Handle(command, cancellationToken));
+            mockRepository.Verify(f => f.Delete(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_InputDataIsInvalid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IFlightRepository>(MockBehavior.Strict);
+
+            var command = new DeleteFlightCommand(-9);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            var handler = new DeleteFlightCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(command, cancellationToken));
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Flights/Commands/DeleteFlight/DeleteFlightCommandValidatorTest.cs
@@ -0,0 +1,48 @@
+﻿using AirportProject.Application.Flights.Commands.DeleteFlight;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Flights.Commands.DeleteFlight
+{
+    [TestClass]
+    public class DeleteFlightCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_DataIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var command = new DeleteFlightCommand(87);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_DataIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new DeleteFlightCommand(-87);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_IdIsZero_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new DeleteFlightCommand(0);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
