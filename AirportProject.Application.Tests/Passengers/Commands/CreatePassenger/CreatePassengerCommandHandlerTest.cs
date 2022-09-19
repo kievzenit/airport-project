1db0142af588a54a0b693698f8c92c021787eb24
kievzenit <contact@kievzenit.tk>
2022-09-19 17:40:14 +0300

    CreatePassengerCommand handler and validator tests

---
+++ AirportProject.Application.Tests/Passengers/Commands/CreatePassenger/CreatePassengerCommandHandlerTest.cs
@@ -0,0 +1,147 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.DTOs;
+using AirportProject.Application.Passengers.Commands.CreatePassenger;
+using AirportProject.Application.Passengers.Queries.GetPassengerByPassport;
+using AirportProject.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.CreatePassenger
+{
+    [TestClass]
+    public class CreatePassengerCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerDoesNotExists_Then_ShoulReturnPassengerDTO()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Passenger, PassengerDTO>>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jason",
+                Passport = "ad367822",
+                Nationality = "American",
+                Gender = "male",
+                Birthday = DateTime.Parse("22/05/19")
+            };
+            var passenger = new Passenger
+            {
+                Id = 9,
+                Firstname = command.Firstname,
+                Lastname = command.Lastname,
+                Passport = command.Passport,
+                Nationality = command.Nationality,
+                Gender = command.Gender,
+                Birthday = command.Birthday
+            };
+            var passengerDTO = new PassengerDTO
+            {
+                Id = passenger.Id,
+                Firstname = passenger.Firstname,
+                Lastname = passenger.Lastname,
+                Passport = passenger.Passport,
+                Nationality = passenger.Nationality,
+                Gender = passenger.Gender,
+                Birthday = passenger.Birthday
+            };
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.Create(command, cancellationToken)).ReturnsAsync(passenger);
+            mockRepository.Setup(f =>
+                f.SearchByPassport(It.IsAny<GetPassengerByPassportQuery>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(default(Passenger));
+
+            mockCaster.Setup(f => f.Cast(passenger, cancellationToken)).ReturnsAsync(passengerDTO);
+
+            var handler = new CreatePassengerCommandHandler(mockRepository.Object, mockCaster.Object);
+
+            var expectedResult = passengerDTO;
+
+            // act
+            var actualResult = handler.Handle(command, cancellationToken).Result;
+
+            // assert
+            Assert.AreEqual(expectedResult, actualResult);
+            mockRepository.Verify(f => f.Create(command, cancellationToken), Times.Once);
+            mockRepository.Verify(f =>
+                f.SearchByPassport(It.IsAny<GetPassengerByPassportQuery>(), It.IsAny<CancellationToken>()),
+                Times.Once);
+            mockCaster.Verify(f => f.Cast(passenger, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerExists_Then_ShoulThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Passenger, PassengerDTO>>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jason",
+                Passport = "ad367822",
+                Nationality = "American",
+                Gender = "male",
+                Birthday = DateTime.Parse("22/05/19")
+            };
+            var existingPassenger = new Passenger
+            {
+                Id = 9,
+                Firstname = "Elvira",
+                Lastname = "Jason",
+                Passport = "ad367822",
+                Nationality = "American",
+                Gender = "male",
+                Birthday = DateTime.Parse("22/05/19")
+            };
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f =>
+                f.SearchByPassport(It.IsAny<GetPassengerByPassportQuery>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(existingPassenger);
+
+            var handler = new CreatePassengerCommandHandler(mockRepository.Object, mockCaster.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(command, cancellationToken))
+                .Wait();
+
+            mockRepository.Verify(f =>
+                f.SearchByPassport(It.IsAny<GetPassengerByPassportQuery>(), It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsInValid_Then_ShoulThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Passenger, PassengerDTO>>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = string.Empty,
+                Lastname = new string('a', 52),
+                Passport = "a",
+                Nationality = string.Empty,
+                Gender = "notexisting",
+                Birthday = DateTime.MaxValue
+            };
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            var handler = new CreatePassengerCommandHandler(mockRepository.Object, mockCaster.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(command, cancellationToken)).Wait();
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Passengers/Commands/CreatePassenger/CreatePassengerCommandValidatorTest.cs
@@ -0,0 +1,304 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Passengers.Commands.CreatePassenger;
+using AirportProject.Application.Passengers.Queries.GetPassengerByPassport;
+using AirportProject.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.CreatePassenger
+{
+    [TestClass]
+    public class CreatePassengerCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_CommandIsValidAndPassengerDoesNotExist_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            mockRepository.Setup(f =>
+                f.SearchByPassport(It.IsAny<GetPassengerByPassportQuery>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(default(Passenger));
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsTrue(result);
+            mockRepository.Verify(f =>
+                f.SearchByPassport(It.IsAny<GetPassengerByPassportQuery>(), It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_CommandIsValidAndPassengerExists_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var existingPassenger = new Passenger
+            {
+                Id = 987,
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            mockRepository.Setup(f =>
+                f.SearchByPassport(It.IsAny<GetPassengerByPassportQuery>(), It.IsAny<CancellationToken>()))
+                    .ReturnsAsync(existingPassenger);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+            mockRepository.Verify(f =>
+                f.SearchByPassport(It.IsAny<GetPassengerByPassportQuery>(), It.IsAny<CancellationToken>()),
+                Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_FirstnameIsTooLong_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = new string('a', 51),
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_FirstnameIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = string.Empty,
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_LastnameIsTooLong_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = new string('a', 51),
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_LastnameIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = string.Empty,
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_PassportIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_NationalityIsTooLong_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = new string('a', 51),
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_NetionalityIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = String.Empty,
+                Gender = "female",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_GenderIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "notexisting",
+                Birthday = DateTime.Parse("22/07/19")
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_BrthdayIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new CreatePassengerCommand
+            {
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American",
+                Gender = "female",
+                Birthday = DateTime.MaxValue
+            };
+
+            // act
+
+            var result = command.IsValid(mockRepository.Object);
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
