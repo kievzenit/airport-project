f041af02031671a8b823c42addd594bcc62bd24e
kievzenit <contact@kievzenit.tk>
2022-09-19 18:55:35 +0300

    DeletePassengerCommand handler and validator tests

---
+++ AirportProject.Application.Tests/Passengers/Commands/DeletePassenger/DeletePassengerCommandHandlerTest.cs
@@ -0,0 +1,71 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.Exceptions;
+using AirportProject.Application.Passengers.Commands.DeletePassenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.DeletePassenger
+{
+    [TestClass]
+    public class DeletePassengerCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerExists_Then_ShouldFinishedWithoutExceptions()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new DeletePassengerCommand(7);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.Delete(command, cancellationToken)).ReturnsAsync(true);
+
+            var handler = new DeletePassengerCommandHandler(mockRepository.Object);
+
+            // act
+            var result = handler.Handle(command, cancellationToken).Result;
+
+            // assert
+            Assert.IsNotNull(result);
+            mockRepository.Verify(f => f.Delete(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerDoesNotExist_Then_ShouldThrowNotFoundException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new DeletePassengerCommand(7);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.Delete(command, cancellationToken)).ReturnsAsync(false);
+
+            var handler = new DeletePassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<NotFoundException>(() => handler.Handle(command, cancellationToken))
+                .Wait();
+            mockRepository.Verify(f => f.Delete(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsInvalid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new DeletePassengerCommand(-87);
+            var cancellationToken = new CancellationTokenSource().Token;
+            var handler = new DeletePassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(() => handler.Handle(command, cancellationToken))
+                .Wait();
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Passengers/Commands/DeletePassenger/DeletePassengerCommandValidatorTest.cs
@@ -0,0 +1,48 @@
+﻿using AirportProject.Application.Passengers.Commands.DeletePassenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.DeletePassenger
+{
+    [TestClass]
+    public class DeletePassengerCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_CommandIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var command = new DeletePassengerCommand(4);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_IdIsNegative_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new DeletePassengerCommand(-4);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_IdIsZero_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new DeletePassengerCommand(0);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
