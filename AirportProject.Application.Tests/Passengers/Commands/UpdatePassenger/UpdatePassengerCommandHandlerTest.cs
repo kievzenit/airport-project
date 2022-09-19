70c4d7f043796b695405632ceb3d077134028540
kievzenit <contact@kievzenit.tk>
2022-09-19 18:05:53 +0300

    UpdatePassengerCommand handler and validator tests

---
+++ AirportProject.Application.Tests/Passengers/Commands/UpdatePassenger/UpdatePassengerCommandHandlerTest.cs
@@ -0,0 +1,94 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.Exceptions;
+using AirportProject.Application.Passengers.Commands.UpdatePassenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.UpdatePassenger
+{
+    [TestClass]
+    public class UpdatePassengerCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerExists_Then_ShoulReturnTrue()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new UpdatePassengerCommand
+            {
+                Id = 99,
+                Firstname = "Elvira",
+                Lastname = "Jason",
+                Passport = "ad367822",
+                Nationality = "American"
+            };
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.Update(command, cancellationToken)).ReturnsAsync(true);
+
+            var handler = new UpdatePassengerCommandHandler(mockRepository.Object);
+
+            // act
+            var result = handler.Handle(command, cancellationToken).Result;
+
+            // assert
+            Assert.IsNotNull(result);
+            mockRepository.Verify(f => f.Update(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerDoesNotExist_Then_ShoulThrowNotFoundException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = "Elvira",
+                Lastname = "Jason",
+                Passport = "ad367822",
+                Nationality = "American"
+            };
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.Update(command, cancellationToken)).ReturnsAsync(false);
+
+            var handler = new UpdatePassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<NotFoundException>(
+                () => handler.Handle(command, cancellationToken)).Wait();
+            mockRepository.Verify(f => f.Update(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsInValid_Then_ShoulThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            var command = new UpdatePassengerCommand
+            {
+                Id = -76,
+                Firstname = string.Empty,
+                Lastname = new string('a', 52),
+                Passport = "a",
+                Nationality = string.Empty
+            };
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            var handler = new UpdatePassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => handler.Handle(command, cancellationToken)).Wait();
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Passengers/Commands/UpdatePassenger/UpdatePassengerCommandValidatorTest.cs
@@ -0,0 +1,209 @@
+﻿using AirportProject.Application.Passengers.Commands.UpdatePassenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.UpdatePassenger
+{
+    [TestClass]
+    public class UpdatePassengerCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_CommandIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_IdIsNegative_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = -98,
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_IdIsZero_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 0,
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_FirstnameIsTooLong_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = new string('a', 51),
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_FirstnameIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = string.Empty,
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = "American"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_LastnameIsTooLong_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = "Elvira",
+                Lastname = new string('a', 51),
+                Passport = "aa991389",
+                Nationality = "American"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_LastnameIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = "Elvira",
+                Lastname = string.Empty,
+                Passport = "aa991389",
+                Nationality = "American"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_PassportIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa",
+                Nationality = "American"
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_NationalityIsTooLong_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = new string('a', 51)
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_NationalityIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var command = new UpdatePassengerCommand
+            {
+                Id = 98,
+                Firstname = "Elvira",
+                Lastname = "Jacob",
+                Passport = "aa991389",
+                Nationality = string.Empty
+            };
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
