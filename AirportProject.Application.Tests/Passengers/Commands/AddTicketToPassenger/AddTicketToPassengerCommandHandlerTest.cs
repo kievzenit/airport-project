8632ff16ca64a2460f226b52440fe5b45f375587
kievzenit <contact@kievzenit.tk>
2022-09-27 14:59:41 +0300

    One more test added

--- AirportProject.Application.Tests/Passengers/Commands/AddTicketToPassenger/AddTicketToPassengerCommandHandlerTest.cs
+++ AirportProject.Application.Tests/Passengers/Commands/AddTicketToPassenger/AddTicketToPassengerCommandHandlerTest.cs
@@ -1,4 +1,5 @@
 ﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.Exceptions;
 using AirportProject.Application.Passengers.Commands.AddTicketToPassenger;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -31,6 +32,28 @@ namespace AirportProject.Application.Tests.Passengers.Commands.AddTicketToPassen
 
             // assert
             Assert.IsNotNull(result);
+            mockRepository.Verify(f => f.AddTicket(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerOrTicketDoNotExist_Then_ShouldThrowNotFoundException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            const int passengerId = 23;
+            const int ticketId = 862;
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.AddTicket(command, cancellationToken)).ReturnsAsync(false);
+
+            var handler = new AddTicketToPassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<NotFoundException>(
+                () => handler.Handle(command, cancellationToken)).Wait();
             mockRepository.Verify(f => f.AddTicket(command, cancellationToken), Times.Once);
         }
 
