487d9e906393c9bd1f2336c626db558db9d3a230
kievzenit <contact@kievzenit.tk>
2022-09-27 15:00:01 +0300

    RemoveRicketFromPassengerCommand handler and validator test

---
+++ AirportProject.Application.Tests/Passengers/Commands/RemoveTicketFromPassenger/RemoveTicketFromPassengerCommandHandlerTest.cs
@@ -0,0 +1,79 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.Exceptions;
+using AirportProject.Application.Passengers.Commands.RemoveTicketFromPassenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.RemoveTicketFromPassenger
+{
+    [TestClass]
+    public class RemoveTicketFromPassengerCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerDoNotHaveThisTicket_Then_ShouldFinishedWithoutExceptions()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            const int passengerId = 23;
+            const int ticketId = 862;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.DeleteTicket(command, cancellationToken)).ReturnsAsync(true);
+
+            var handler = new RemoveTicketFromPassengerCommandHandler(mockRepository.Object);
+
+            // act
+            var result = handler.Handle(command, cancellationToken);
+
+            // assert
+            Assert.IsNotNull(result);
+            mockRepository.Verify(f => f.DeleteTicket(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerOrTicketDoNotExist_Then_ShouldThrowNotFoundException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            const int passengerId = 23;
+            const int ticketId = 862;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.DeleteTicket(command, cancellationToken)).ReturnsAsync(false);
+
+            var handler = new RemoveTicketFromPassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<NotFoundException>(
+                () => handler.Handle(command, cancellationToken)).Wait();
+            mockRepository.Verify(f => f.DeleteTicket(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsInvalid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            const int passengerId = -23;
+            const int ticketId = -862;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            var handler = new RemoveTicketFromPassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => handler.Handle(command, cancellationToken)).Wait();
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Passengers/Commands/RemoveTicketFromPassenger/RemoveTicketFromPassengerCommandValidatorTest.cs
@@ -0,0 +1,99 @@
+﻿using AirportProject.Application.Passengers.Commands.RemoveTicketFromPassenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.RemoveTicketFromPassenger
+{
+    [TestClass]
+    public class RemoveTicketFromPassengerCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_CommandIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            const int passengerId = 386;
+            const int ticketId = 876;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_CommandIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = -386;
+            const int ticketId = -876;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_PassengerIdIsNegative_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = -386;
+            const int ticketId = 876;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_PassengerIdIsZero_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = 0;
+            const int ticketId = 876;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_TicketIdIsNegative_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = 386;
+            const int ticketId = -876;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_TicketIdIsZero_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = 386;
+            const int ticketId = 0;
+            var command = new RemoveTicketFromPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
