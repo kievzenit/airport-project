44ec728dabf1f002fdff779985f0300bb1dd5c45
kievzenit <contact@kievzenit.tk>
2022-09-27 14:27:44 +0300

    AddTicketToPassengerCommand validator and handler tests

---
+++ AirportProject.Application.Tests/Passengers/Commands/AddTicketToPassenger/AddTicketToPassengerCommandHandlerTest.cs
@@ -0,0 +1,79 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Passengers.Commands.AddTicketToPassenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.AddTicketToPassenger
+{
+    [TestClass]
+    public class AddTicketToPassengerCommandHandlerTest
+    {
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerDoNotHaveThisTicket_Then_ShouldFinishedWithoutExceptions()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            const int passengerId = 23;
+            const int ticketId = 862;
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.AddTicket(command, cancellationToken)).ReturnsAsync(true);
+
+            var handler = new AddTicketToPassengerCommandHandler(mockRepository.Object);
+
+            // act
+            var result = handler.Handle(command, cancellationToken);
+
+            // assert
+            Assert.IsNotNull(result);
+            mockRepository.Verify(f => f.AddTicket(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsValidAndPassengerHaveThisTicket_Then_ShouldThrowInvalidOperationException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            const int passengerId = 23;
+            const int ticketId = 862;
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.AddTicket(command, cancellationToken))
+                .ThrowsAsync(new InvalidOperationException());
+
+            var handler = new AddTicketToPassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<InvalidOperationException>(
+                () => handler.Handle(command, cancellationToken)).Wait();
+            mockRepository.Verify(f => f.AddTicket(command, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_CommandHandler_When_CommandIsInvalid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<IPassengerRepository>(MockBehavior.Strict);
+
+            const int passengerId = -23;
+            const int ticketId = -862;
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            var handler = new AddTicketToPassengerCommandHandler(mockRepository.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => handler.Handle(command, cancellationToken)).Wait();
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Passengers/Commands/AddTicketToPassenger/AddTicketToPassengerCommandValidatorTest.cs
@@ -0,0 +1,105 @@
+﻿using AirportProject.Application.Passengers.Commands.AddTicketToPassenger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Passengers.Commands.AddTicketToPassenger
+{
+    [TestClass]
+    public class AddTicketToPassengerCommandValidatorTest
+    {
+        [TestMethod]
+        public void Test_CommandValidator_When_CommandIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            const int passengerId = 34;
+            const int ticketId = 387;
+
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_CommandIsInvalid_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = -34;
+            const int ticketId = -387;
+
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_PassengerIdIsNegative_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = -34;
+            const int ticketId = 387;
+
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_PassengerIdIsZero_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = 0;
+            const int ticketId = 387;
+
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_TicketIdIsNegative_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = 34;
+            const int ticketId = -387;
+
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_CommandValidator_When_TicketIdIsZero_Then_ShouldReturnFalse()
+        {
+            // arrange
+            const int passengerId = 34;
+            const int ticketId = 0;
+
+            var command = new AddTicketToPassengerCommand(passengerId, ticketId);
+
+            // act
+            var result = command.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
