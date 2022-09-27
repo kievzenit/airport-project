ad2470d181e32aadcf8e5f4bb238b0bb3622e7dd
kievzenit <contact@kievzenit.tk>
2022-09-27 17:22:07 +0300

    GetSpecificTicketsQuery handler and validator tests

---
+++ AirportProject.Application.Tests/Tickets/Queries/GetSpecificTickets/GetSpecificTicketsQueryHandlerTest.cs
@@ -0,0 +1,72 @@
+﻿using AirportProject.Application.Common.Abstract;
+using AirportProject.Application.Common.DTOs;
+using AirportProject.Application.Tickets.Queries.GetSpecificTickets;
+using AirportProject.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AirportProject.Application.Tests.Tickets.Queries.GetSpecificTickets
+{
+    [TestClass]
+    public class GetSpecificTicketsQueryHandlerTest
+    {
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryIsValid_Then_ShouldFinishedWithoutExceptions()
+        {
+            // arrange
+            var mockRepository = new Mock<ITicketRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Ticket, TicketDTO>>(MockBehavior.Strict);
+
+            var query = new GetSpecificTicketsQuery
+            {
+                From = "Kiev",
+                To = "Berlin",
+                Type = "economy"
+            };
+            var tickets = new List<Ticket>();
+            var ticketDTOs = new List<TicketDTO>();
+
+            var cancellationToken = new CancellationTokenSource().Token;
+
+            mockRepository.Setup(f => f.GetTickets(query, cancellationToken)).ReturnsAsync(tickets);
+            mockCaster.Setup(f => f.Cast(tickets, cancellationToken)).ReturnsAsync(ticketDTOs);
+
+            var handler = new GetSpecificTicketsQueryHandler(mockRepository.Object, mockCaster.Object);
+
+            var expectedResult = ticketDTOs;
+
+            // act
+            var actualResult = handler.Handle(query, cancellationToken).Result;
+
+            // assert
+            Assert.AreEqual(expectedResult, actualResult);
+            mockRepository.Verify(f => f.GetTickets(query, cancellationToken), Times.Once);
+            mockCaster.Verify(f => f.Cast(tickets, cancellationToken), Times.Once);
+        }
+
+        [TestMethod]
+        public void Test_QueryHandler_When_QueryIsInvalid_Then_ShouldThrowArgumentException()
+        {
+            // arrange
+            var mockRepository = new Mock<ITicketRepository>(MockBehavior.Strict);
+            var mockCaster = new Mock<ICaster<Ticket, TicketDTO>>(MockBehavior.Strict);
+
+            var query = new GetSpecificTicketsQuery
+            {
+                From = new string('a', 51),
+                To = string.Empty,
+                Type = "doesnotexisting"
+            };
+            var cancellationToken = new CancellationTokenSource().Token;
+            var handler = new GetSpecificTicketsQueryHandler(mockRepository.Object, mockCaster.Object);
+
+            // assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(
+                () => handler.Handle(query, cancellationToken))
+                .Wait();
+        }
+    }
+}
---
+++ AirportProject.Application.Tests/Tickets/Queries/GetSpecificTickets/GetSpecificTicketsQueryValidatorTest.cs
@@ -0,0 +1,135 @@
+﻿using AirportProject.Application.Tickets.Queries.GetSpecificTickets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirportProject.Application.Tests.Tickets.Queries.GetSpecificTickets
+{
+    [TestClass]
+    public class GetSpecificTicketsQueryValidatorTest
+    {
+        [TestMethod]
+        public void Test_QueryValidator_When_QueryIsValid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var query = new GetSpecificTicketsQuery
+            {
+                From = "Kiev",
+                To = "Berlin",
+                Type = "economy"
+            };
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryValidator_When_FromValueIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var query = new GetSpecificTicketsQuery
+            {
+                From = string.Empty,
+                To = "Berlin",
+                Type = "economy"
+            };
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryValidator_When_FromValueIsTooLong_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var query = new GetSpecificTicketsQuery
+            {
+                From = new string('a', 51),
+                To = "Berlin",
+                Type = "economy"
+            };
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryValidator_When_ToValueIsEmpty_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var query = new GetSpecificTicketsQuery
+            {
+                From = "Keiv",
+                To = string.Empty,
+                Type = "economy"
+            };
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryValidator_When_ToValueIsTooLong_Then_ShouldReturnFalse()
+        {
+            // arrange
+            var query = new GetSpecificTicketsQuery
+            {
+                From = "Keiv",
+                To = new string('a', 51),
+                Type = "economy"
+            };
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryValidator_When_TypeIsEmpty_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var query = new GetSpecificTicketsQuery
+            {
+                From = "Kiev",
+                To = "Berlin",
+                Type = string.Empty
+            };
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_QueryValidator_When_TypeIsInvalid_Then_ShouldReturnTrue()
+        {
+            // arrange
+            var query = new GetSpecificTicketsQuery
+            {
+                From = "Kiev",
+                To = "Berlin",
+                Type = "doesnotexisting"
+            };
+
+            // act
+            var result = query.IsValid();
+
+            // assert
+            Assert.IsFalse(result);
+        }
+    }
+}
